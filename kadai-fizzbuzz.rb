a23a842555b40cc7c9cb1018f2fc9f8f1189a8f7
fujitakeiei <masakazu777@ymail.ne.jp>
2024-02-23 14:33:24 +0000

    first fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,19 @@
+num_max = 100
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return num
+  end
+end
+
+# 以下は動作確認用の記述です
+puts fizzbuzz(1)
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
