281a90638a1c2a5a90ae35d9c0c9eaa5d433eecd
fujitakeiei <masakazu777@ymail.ne.jp>
2024-02-23 14:48:43 +0000

    num_max100 commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,5 +1,4 @@
-num_max = 100
-
+=begin
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
@@ -17,3 +16,23 @@ puts fizzbuzz(1)
 puts fizzbuzz(3)
 puts fizzbuzz(5)
 puts fizzbuzz(15)
+=end
+
+
+num_max = 100
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return num
+  end
+end
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
